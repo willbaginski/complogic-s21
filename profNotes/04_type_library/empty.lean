eeb715407172c7b83f2ae6be955abe5285d4cbfa
Kevin Sullivan <sullivan.kevinj@gmail.com>
2021-02-09 20:40:59 -0500

    Initialized most of a standard data type library

---
+++ profNotes/04_type_library/empty.lean
@@ -0,0 +1,14 @@
+namespace hidden
+
+/-
+The empty data type has no
+values/constructors at all.
+This fact becomes interesting
+and useful when one performs
+a case analysis on values of
+this type, as there are no
+cases to consider. 
+-/
+inductive empty : Type
+
+end hidden
