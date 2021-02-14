24558265c7b6a2f326b693b81ab2c35b2d6bb0fc
Kevin Sullivan <sullivan.kevinj@gmail.com>
2021-02-13 20:10:46 -0500

    Adding material on higher order functions, with map for options and boxes as an example. Also found missing material on nat type misplaced in option file! Oops.

---
+++ inClassNotes/type_library/option_test.lean
@@ -0,0 +1,11 @@
+namespace hidden 
+
+/-
+EXERCISE: Represent the partial function
+just described using the option type as
+just explained. Write your definition in
+a separate file, option_test.lean.
+-/
+
+end hidden
+
