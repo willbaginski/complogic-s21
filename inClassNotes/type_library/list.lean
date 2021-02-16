0937b5ae4f08bfd0b18f650a9150bd464ccf5930
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-02-16 07:44:41 -0500

    Update type library for class prep

--- inClassNotes/type_library/list.lean
+++ inClassNotes/type_library/list.lean
@@ -1,20 +1,11 @@
-#print nat
-
 namespace hidden
-
-/-
-Here's Lean's definition of the
-list type builder. We'll use it
-instead of our own version.
 
 universe u
 
 inductive list (α : Type u) : Type u
-| nil : list
-| cons : α → list → list 
--/
-
-#print list 
+| nil {} : list
+| cons  (h : α) (t : list) : list
 
 end hidden
 
+#print list
